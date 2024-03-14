2735e61992a52c7e82b81d5396ea9411af90d422
kabirt7 <kabir.t7@hotmail.com>
2024-03-14 12:45:52 +1100

    initalise spring

---
+++ postcode-api/src/main/java/io/nology/postcodeapi/PostcodeApiApplication.java
@@ -0,0 +1,13 @@
+package io.nology.postcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcodeApiApplication.class, args);
+	}
+
+}
