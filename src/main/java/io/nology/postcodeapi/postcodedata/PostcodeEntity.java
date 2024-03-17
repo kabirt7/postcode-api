9401dfdbf126ecc4e53a715858feea7554a09550
kabirt7 <kabir.t7@hotmail.com>
2024-03-17 14:55:31 +1100

    Entity created

---
+++ src/main/java/io/nology/postcodeapi/postcodedata/PostcodeEntity.java
@@ -0,0 +1,92 @@
+package io.nology.postcodeapi.postcodedata;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "postcode_data")
+public class PostcodeEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private Long postcode;
+	
+	@Column
+	private String suburb;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column (nullable = false, updatable = false)
+	private Date createdAt;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column
+	private Date updatedAt;
+	
+	@PrePersist
+	public void onCreate() {
+		Date timestamp = new Date();
+		createdAt = timestamp;
+		updatedAt = timestamp;
+	}
+	
+	@PreUpdate
+	public void onUpdate() {
+		updatedAt = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Long getPostcode() {
+		return postcode;
+	}
+
+	public String getSuburb() {
+		return suburb;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setPostcode(Long postcode) {
+		this.postcode = postcode;
+	}
+
+	public void setSuburb(String suburb) {
+		this.suburb = suburb;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+}
+
+
